5a462997e7018188a9deef718d5ee46bf2d903c3
JSHP9 <127930477+JSHP9@users.noreply.github.com>
2026-04-15 18:35:15 +0900

    [Silver IV] Title: 괄호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.cc
+++ 백준/Silver/9012. 괄호/괄호.cc
@@ -24,10 +24,7 @@ int main()
 				myStack.push(input[i]);
 			}
 			//')'가 나오면 스택에 저장된 '('와 짝이 맞는지 확인하고 pop
-            // 스택이 비어있을 때 호출되면 정의되지 않은 동작을 유발함
-            // 즉 스택이 비어있는데 top을 호출하면 프로그램이 충돌함 그래서 empty()를 추가해야 제대로 동작함
-			// 요약: !myStack.empty()는 stack이 비어 있는 상태에서 top()호출 방지 용도로 사용
-            else if (!myStack.empty() && input[i] == ')' && myStack.top() == '(') {
+			else if (!myStack.empty() && input[i] == ')' && myStack.top() == '(') {
 				myStack.pop();
 			}
 			//위의 경우를 제외하고는 모두 vps가 아닌 상황이므로 종료(break)
