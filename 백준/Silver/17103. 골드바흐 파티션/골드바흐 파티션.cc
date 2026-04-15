0e1026458b0fe3259e566ac0a2b83574b273d5ef
JSHP9 <127930477+JSHP9@users.noreply.github.com>
2026-04-15 18:39:53 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 32 ms, Memory: 3040 KB -BaekjoonHub

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.cc
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.cc
@@ -6,7 +6,7 @@ using std::cout, std::cin;
 // 에라토스테네스의 체
 std::vector<int> sieveOfEratosthenes(int n);
 // 소수 합 찾기
-int countPrimePairs(int n, std::vector<int> primes);
+int countPrimePairs(int n, const std::vector<int>& primes);
 
 int main() 
 {
@@ -17,24 +17,23 @@ int main()
     int t, maxN = 0;
     cin >> t;
     std::vector<int> testCases(t);
-    for (size_t i = 0; i < t; ++i) {
+
+    for (int i = 0; i < t; ++i) {
         cin >> testCases[i];
-        // 가장 큰 N을 구함
-        if (maxN < testCases[i])
+        if (testCases[i] > maxN) {
             maxN = testCases[i];
+        }
     }
 
-    // 가장 큰 N에 대해 한번만 소수 계산
+    // 가장 큰 n에 대해 한 번만 소수를 계산
     std::vector<int> primes = sieveOfEratosthenes(maxN);
     for (int n : testCases) {
         if (n == 1) {
             cout << 0 << '\n';
             continue;
         }
-        // (숫자 n, 소수 모음집)
         cout << countPrimePairs(n, primes) << '\n';
     }
-    
     return 0;
 }
 
@@ -45,37 +44,39 @@ std::vector<int> sieveOfEratosthenes(int n)
     std::vector<int> primes;
 
     isPrime[0] = isPrime[1] = false;
-    for (size_t i = 2; i * i <= n; ++i) {
+    for (int i = 2; i * i <= n; ++i) {
         if (isPrime[i]) {
-            for (size_t j = i * i; j <= n; j += i)
+            for (int j = i * i; j <= n; j += i) {
                 isPrime[j] = false;
+            }
         }
     }
 
-    for (size_t i = 2; i < n; ++i) 
-        if(isPrime[i])
+    for (int i = 2; i <= n; ++i) {
+        if (isPrime[i]) {
             primes.push_back(i);
-
+        }
+    }
     return primes;
 }
 
-int countPrimePairs(int n, std::vector<int> primes) 
+int countPrimePairs(int n, const std::vector<int>& primes) 
 {
     int cnt = 0;
     int left = 0, right = primes.size() - 1;
+    // 투 포인터 방식으로 소수의 합을 찾음
     while (left <= right) {
-        // 투 포인터 방식으로 소수의 합을 찾음
         int sum = primes[left] + primes[right];
-        if (n == sum) {
+        if (sum == n) {
             ++cnt;
             ++left;
             --right;
-        }
-        else if (n < sum) {
-            --right;
-        }
-        else {
+        } 
+        else if (sum < n) {
             ++left;
+        } 
+        else {
+            --right;
         }
     }
     return cnt;
