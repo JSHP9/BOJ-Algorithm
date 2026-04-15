7a4b099be0df7861b7be3dbae1cf7c1be29aaa3b
JSHP9 <127930477+JSHP9@users.noreply.github.com>
2026-04-15 17:40:59 +0900

    [Silver III] Title: 분수 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1735. 분수 합/분수 합.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <numeric>
+
+using std::cout, std::cin;
+
+int main() 
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    int con[2] = {0};
+    int den[2] = {0};
+    int c, d;
+    for (int i = 0; i < 2; ++i) {
+        cin >> c >> d;
+        con[i] = c;
+        den[i] = d;
+    }
+
+    int cn = (con[0] * den[1]) + (con[1] * den[0]);
+    int dn = den[0] * den[1];
+
+    // 최대공약수
+    int g = std::gcd(cn, dn);
+
+    // 최대공약수로 약분
+    cout << cn / g << " " << dn / g;
+    
+    return 0;
+}
