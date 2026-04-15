20e0330613cfdbbe0e363427875d6202e792b94c
JSHP9 <127930477+JSHP9@users.noreply.github.com>
2026-04-15 18:48:04 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 108 ms, Memory: 3572 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -1,27 +1,19 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
-using std::cin, std::cout, std::endl;
-
-int main() 
-{
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(NULL);
-
-    int n, x, y;
-    cin >> n;
-    std::vector<std::pair<int, int>> posXY;
-
-    for (size_t i = 0; i < n; ++i) {
-        cin >> x >> y;
-        posXY.push_back({x, y});
-    }
-
-    sort(posXY.begin(), posXY.end());
-    
-    for (size_t i = 0; i < posXY.size(); ++i)
-        cout << posXY[i].first <<' ' << posXY[i].second << '\n';
-    
-    return 0;
+using namespace std;
+vector<pair<int, int>> v;
+int main() {
+	int N;
+	cin >> N;
+	int x, y;
+	for (int i = 0; i < N; i++) {
+		cin >> x >> y;
+		v.push_back({ x, y });
+	}
+	sort(v.begin(), v.end());
+	for (int i = 0; i < N; i++) {
+		cout << v[i].first << ' ' << v[i].second << '\n';
+	}
+	return 0;
 }
