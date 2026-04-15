66df2d3cf2d915d092079e574795cad5c1bd6ee2
JSHP9 <127930477+JSHP9@users.noreply.github.com>
2026-04-15 18:31:10 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2208 KB, Score: 50 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -9,7 +9,7 @@ int main()
     cin.tie(nullptr);
     cout.tie(nullptr);
 
-    long long r = 1, m = 1234567891;
+    long long r = 31, m = 1234567891;
     long long l, sum = 0;
     std::string str;
     
@@ -18,9 +18,7 @@ int main()
 
     for (int i = 0; i < l; ++i) {
         // a가 97이기 떄문에 96 빼줌
-        sum += ((long(str[i]) - 96) * r) % m;
-        // m이 있는 이유는 자료형의 범위(1234567891)를 넘어가는 값을 다시 초기화시켜주기 위함
-        r = (r * 31) % m;
+        sum += (str[i] - 96) * pow(r, i);
     }
 
     cout << sum % m;
