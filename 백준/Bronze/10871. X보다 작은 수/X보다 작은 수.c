5a5a59ede540b7a09607283ccdabf516146740fe
JSHP9 <127930477+JSHP9@users.noreply.github.com>
2026-04-15 19:38:19 +0900

    [Bronze V] Title: X보다 작은 수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.c
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.c
@@ -4,14 +4,15 @@
 int main() {
     int n, x;
     scanf("%d %d", &n, &x);
-
-    int a;
+    
+    int *a = (int *)malloc(n * sizeof(int));
     for (int i = 0; i < n; i++)
     {
-        scanf("%d", &a);
-        if (a < x)
-            printf("%d ", a);
+        scanf("%d", &a[i]);
+        if (a[i] < x)
+            printf("%d ", a[i]);
         
     }
+    free(a);
     return 0;
 }
