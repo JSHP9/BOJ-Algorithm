95084a4b5c9a3a61b919310a99ba216226051b1f
JSHP9 <127930477+JSHP9@users.noreply.github.com>
2026-04-15 17:24:42 +0900

    [Bronze V] Title: A-B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+class Sum {
+	int x, y;
+public:
+	void add() {
+		cin >> x >> y;
+		cout << x - y;
+	}
+};
+
+int main()
+{
+	Sum a;
+	a.add();
+	return 0;
+}
