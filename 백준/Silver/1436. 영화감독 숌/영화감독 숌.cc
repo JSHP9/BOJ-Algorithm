816eb464fffc5ab8126ecf0b262f8b7c5b30771e
JSHP9 <127930477+JSHP9@users.noreply.github.com>
2026-04-15 18:59:50 +0900

    [Silver V] Title: 영화감독 숌, Time: 36 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
@@ -4,27 +4,25 @@ using namespace std;
 
 int main() 
 {
-    ios::sync_with_stdio(false); // c, c++ 표준 입출력 동기화 해제
-    cin.tie(NULL); // 입력과 출력 묶음 풀기
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
     
     int n, end = 666, endCnt = 0;
-    int endCp = end; // end값 복사본
+    int endCp = end;
     cin >> n;
     for (int i = 0; i < n; ++i) {
         while (endCp) {
-            // 6이 연속되게 나오는지 확인
             if (endCp % 10 == 6 && (endCp/10) % 10 == 6 && ((endCp/10)/10) % 10 == 6) 
-                endCnt = 3; // 6이 세개 이상이면 되므로 count = 3
+                endCnt = 3;       
             endCp /= 10;
         }
         
-        if (endCnt < 3) --i; // 666이 안나오면 루프 한번 더 돌기
+        if (endCnt < 3) --i;
         
-        // 값 초기화
-        end++; 
+        end++;
         endCp = end;
         endCnt = 0;
     }
-    cout << end - 1; // 마지막에 end값을 1 증가시키기 떄문에 1 감소
+    cout << end - 1;
     return 0;
 }
